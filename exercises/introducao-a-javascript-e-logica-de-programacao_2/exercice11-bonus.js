02a6af54b161e84f7608c73554e459ed8d47b77f
Anderson <trkz.anderson@gmail.com>
2021-03-14 20:20:56 -0300

    Manipulação de array, e algoritmos de ordenação

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercice11-bonus.js
@@ -0,0 +1,16 @@
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index++) {
+  for (let aux = 0; aux < index; aux++) {
+    if (numbers[index] > numbers[aux]) {
+      let position = numbers[index];
+
+      numbers[index] = numbers[aux];
+      numbers[aux] = position;
+    }
+  }
+}
+
+console.log(numbers);
