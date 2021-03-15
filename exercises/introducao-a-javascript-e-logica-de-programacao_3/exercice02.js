059b798fff8faf2c531af99bd31d026ad8fa4b56
Anderson <trkz.anderson@gmail.com>
2021-03-14 21:37:57 -0300

    Desafios com *

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercice02.js
@@ -0,0 +1,16 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+//n = 5
+// *
+// **
+// ***
+// ****
+// *****
+
+let value = 5;
+let text = "";
+
+for(let index = 0; index < value; index++){
+  text = text + "*";
+  console.log(text);
+}
