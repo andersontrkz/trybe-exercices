d7e38643fccdf30980fdcb35ad6cbe3cdd6f3dc4
Anderson <trkz.anderson@gmail.com>
2021-05-30 23:05:43 -0300

    add exercise

---
+++ Module-01-Web-Development-Fundamentals/Block-10-Automated-Tests-with-Jest/Day-01-First-steps-at-Jest-Exercices/Part-01/README.md
@@ -0,0 +1,2 @@
+Parte I
+Refaça os exercícios 1 a 5 do conteúdo de Testes unitários em JavaScript , dessa vez utilizando Jest .
---
+++ Module-01-Web-Development-Fundamentals/Block-10-Automated-Tests-with-Jest/Day-01-First-steps-at-Jest-Exercices/Part-Bonus/exercise-bonus.js
@@ -0,0 +1,17 @@
+// exercise-bonus.js
+
+const searchEmployee = (id, detail) => {
+  const employee = professionalBoard.find(employee => employee.id === id);
+
+  if (!employee) {
+    throw new Error("ID não identificada");
+  };
+  if (!employee[detail]) {
+    throw new Error("Informação indisponível");
+  };
+
+  return employee[detail];
+};
+
+
+module.exports = searchEmployee;
